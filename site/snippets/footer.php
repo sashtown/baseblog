a6f920267429d74c9d5bbda5f7a39446c3f6b734
sashtown <info@slstudio.de>
2012-05-10 20:03:31 +0200

    cleaned up footer snippet

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -9,19 +9,6 @@ Footer Snippet for the Baseblog Kirby Theme
 
   	</div><!-- #site -->
 
-<!-- Piwik --> 
-<script type="text/javascript">
-var pkBaseURL = (("https:" == document.location.protocol) ? "https://stats.slstudio.de/" : "http://stats.slstudio.de/");
-document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-</script><script type="text/javascript">
-try {
-var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 7);
-piwikTracker.trackPageView();
-piwikTracker.enableLinkTracking();
-} catch( err ) {}
-</script><noscript><p><img src="http://stats.slstudio.de/piwik.php?idsite=7" style="border:0" alt="" /></p></noscript>
-<!-- End Piwik Tracking Code -->
-
 </body>
 
 </html>
