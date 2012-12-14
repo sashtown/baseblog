bd2e278623320fb5a745b44ec696c7e5cdb4d78b
Mario Rimann <mario@rimann.org>
2012-12-14 23:30:39 +0100

    [BUGFIX] Replace the PHP short tags
    The short tags could lead to problems with PHP v5.4

--- site/templates/blog.php
+++ site/templates/blog.php
@@ -1,7 +1,7 @@
 <?php snippet('header'); ?>
 
     <?php snippet('menu'); ?>
-		
+
 	<section class="blog">
 
         <?php if(param('category')) {   /*** article overview ***/
@@ -14,51 +14,51 @@
                             ->paginate(3);
 
                             echo ('<h1 class="category_overview">Category Archives: <em>'), (param('category')), ('</em></h1>');
-            } else {                 
+            } else {
 
             $articles = $pages->find('blog')
                             ->children()
                             ->visible()
                             ->flip()
-                            ->paginate(3);  
+                            ->paginate(3);
         }?>
 
-        <? foreach($articles as $article): ?>
+        <?php foreach($articles as $article): ?>
 
         <article>
             <?php if($article->template() == 'article.text'): /*** postformat: TEXT ***/ ?>
             <header class="post-meta">
                 <h1><a href="<?php echo $article->url() ?>"><?php echo html($article->title()) ?></a></h1>
-                Posted on <time datetime="<?php echo $article->date('c') ?>"><?php echo $article->date('d.m.Y'); ?></time> in 
+                Posted on <time datetime="<?php echo $article->date('c') ?>"><?php echo $article->date('d.m.Y'); ?></time> in
                 <a class="category" href="<?php echo url() ?>blog/category:<?php echo $article->categories(); ?>"><?php echo $article->categories(); ?></a>
             </header>
             <p><?php echo excerpt($article->text(), 400) ?></p>
             <a class="read_more" href="<?php echo $article->url() ?>">read more →</a>
-            
+
             <?php elseif($article->template() == 'article.link'): /*** postformat: LINK ***/ ?>
             <header class="post-meta format_link">
                 <h1><a href="<?php echo $article->customlink() ?>"><?php echo html($article->linktitle()) ?></a></h1>
             </header>
             <?php echo kirbytext($article->text()) ?>
             <a class="read_more" href="<?php echo $article->url() ?>">permalink →</a>
-            <?php endif ?>            
+            <?php endif ?>
         </article>
 
         <?php endforeach ?>
 
-                   
-		<? if($articles->pagination()->hasPages()): /*** pagination ***/ ?>
+
+		<?php if($articles->pagination()->hasPages()): /*** pagination ***/ ?>
         <nav class="pagination">
-        
-            <? if($articles->pagination()->hasPrevPage()): ?>
+
+            <?php if($articles->pagination()->hasPrevPage()): ?>
             <a class="prev" href="<?= $articles->pagination()->prevPageURL() ?>">newer posts &lsaquo;&lsaquo;</a>
-            <? endif ?>
-            <? if($articles->pagination()->hasNextPage()): ?>
+            <?php endif ?>
+            <?php if($articles->pagination()->hasNextPage()): ?>
             <a class="next" href="<?= $articles->pagination()->nextPageURL() ?>">&lsaquo;&lsaquo; older posts</a>
-            <? endif ?>
-        
+            <?php endif ?>
+
         </nav>
-        <? endif ?>                           
+        <?php endif ?>
     </section>
-        
+
 <?php snippet('footer') ?>
